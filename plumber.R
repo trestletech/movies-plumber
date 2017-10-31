a4b202f6a3c68066be2f5bb03fe7797df4a7c722
trestletech <jeff.allen@trestletechnology.net>
2017-10-31 11:23:40 -0500

    Added search functionality

--- plumber.R
+++ plumber.R
@@ -2,6 +2,7 @@
 library(dplyr)
 library(readr)
 library(tidyr)
+library(ggplot2)
 
 movies <- readr::read_csv("ml-latest-small/movies.csv")
 ratings <- readr::read_csv("ml-latest-small/ratings.csv")
@@ -11,25 +12,47 @@ links <- readr::read_csv("ml-latest-small/links.csv") %>%
          movielens = paste0("https://movielens.org/movies/", movieId)) %>% 
   select(-tmdbId, -imdbId)
 
+ratedMovies <- ratings %>% 
+  group_by(movieId) %>% 
+  summarize(avgRating = mean(rating), votes = n()) %>% 
+  left_join(movies, by="movieId") %>% 
+  left_join(links, by="movieId")
+
 #' Get the top 10 ranked movies
 #' @get /api/movies/top
 function(){
-  ratings %>% 
-    group_by(movieId) %>% 
-    summarize(avgRating = mean(rating), votes = n()) %>% 
+  ratedMovies %>% 
     filter(votes > 50) %>% # Minimum of 10 votes
     arrange(desc(avgRating)) %>% 
-    head(10) %>% 
-    left_join(movies) %>% 
-    left_join(links, by="movieId")
+    head(10)
 }
 
 #' Return info about a particular move
 #' @get /api/movies/<id:int>
+#' @serializer unboxedJSON
 function(id){
-  mov <- movies %>% filter(movieId == id) %>% 
-    left_join(links, by="movieId")
-  unlist(mov)
+  mov <- ratedMovies %>% filter(movieId == id)
+    
+  as.list(mov)
+}
+
+#' Search for a movie by title
+#' @post /api/search
+function(q){
+  # Alphanumeric and spaces only
+  query <- gsub("[^\\w ]", "", q, perl=TRUE)
+  movies %>% filter(grepl(toupper(query), toupper(title), fixed=TRUE))
+}
+
+#' Histogram of ratings for a particular movie
+#' @png
+#' @get /api/movies/<id:int>/ratings
+function(id){
+  plot <- ratings %>% 
+    filter(movieId == id) %>% 
+    select(rating) %>% 
+    ggplot(aes(rating)) + ggplot2::geom_histogram(binwidth = .5)
+  print(plot)
 }
 
 #' Serve the core HTML file for any request for a page
@@ -40,7 +63,7 @@ function(res){
 
 #' Serve the core HTML file for any request for a page
 #' TODO: collapse into a single endpoint once https://github.com/trestletech/plumber/issues/191
-#' @get /movie/<id>
+#' @get /movies/<id>
 function(res){
   plumber::include_html("./public/index.html", res)
 }
--- public/movies.js
+++ public/movies.js
@@ -1,17 +1,76 @@
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js!',
-    movies: []
+const Home = { 
+  template: '<div><div style="width: 50%; float: left;"><h2>Top 10 Movies</h2><ul><li v-for="m in movies"><router-link :to="m.link">{{ m.title }}</router-link></li></ul></div><search style="float: right; width: 45%;"></search></div>',
+  data: function(){
+    return {
+      movies: []
+    }
+  }, mounted: function(){
+    this.$http.get('/api/movies/top').then(function(res) {
+      this.movies = res.data.map(m => {
+        m.link = '/movies/' + m.movieId
+        return m
+      })
+    }.bind(this), err => {
+      console.log(err);
+    })
+  }
+}
+
+const Movie = { 
+  path:'/movies/:id/', 
+  template: '<div><h2>{{ movie.title }}</h2><img :src="ratingsImg" style="float: left; width: 33%;"></img><iframe width="65%" height="350px" :src="movie.imdb" style="float: right;"></iframe></div>',
+  data: function(){
+    return {
+      movie: {},
+    }
+  }, mounted: function() {
+    this.$http.get('/api/movies/' + this.$route.params.id).then(function(res) {
+      this.movie = res.data;
+    }.bind(this), err => {
+      console.log(err);
+    })
   }, computed: {
-    currentMovie: function() {
-      const movieRe = /^\/movie\/(\d+)$/
-      const curRoute = window.location.pathname
-      if (!movieRe.test(curRoute)){
+    ratingsImg: function(){
+      if (!this.movie){
         return null
       }
-      return curRoute.replace(movieRe, '$1')
-    } 
-  
+      return '/api/movies/' + this.movie.movieId + '/ratings'
+    }
+  }
+}
+
+Vue.component('search', {
+  template:'<div>Search: <input v-model="query"></input><button v-on:click="search">Search</button><ul><li v-for="r in results"><router-link :to="r.link">{{ r.title }}</router-link></li></ul></div>',
+  data: function(){
+    return { 
+      query: '',
+      results: []
+    }
+  },
+  methods: {
+    search: function(){
+      this.$http.post('/api/search?q=' + this.query).then(function(res) {
+        this.results = res.data.map(r => {
+          r.link = '/movies/' + r.movieId
+          return r
+        });
+      }.bind(this), err => {
+        console.log(err);
+      })
+    }
   }
 })
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/movies/:id', component: Movie }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
